8eac93f02805d5a95ce3d17b16a40f36dabdf20e
jianguiqubaa <jianguiqubaa@gmail.com>
2014-10-12 17:05:11 +0800

    refactoring

--- xiami_auto_signin.py
+++ xiami_auto_signin.py
@@ -5,15 +5,15 @@ import urllib
 import re
 
 
-def login(email,pw):
+def login(email, pw):
     login_data_ = {
-    'email': email,
-    'password': pw,
-    'LoginButton': '登录'}
+        'email': email,
+        'password': pw,
+        'LoginButton': '登录'}
     login_url = 'http://www.xiami.com/web/login'
     login_headers = {
-    'Referer': 'http://www.xiami.com/web',
-    'User-Agent': 'Chrome/35.0'}
+        'Referer': 'http://www.xiami.com/web',
+        'User-Agent': 'Chrome/35.0'}
 
     login_data = urllib.urlencode(login_data_)
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor)
@@ -23,16 +23,16 @@ def login(email,pw):
     login_request = urllib2.Request(login_url, login_data, login_headers)
     login_response = urllib2.urlopen(login_request).read()
 
-
     if 'email或者密码错误' in login_response:
         return 0
     else:
-        return days('http:www.xiami.com/profile','http://www.xiami.com/web')
+        return days('http:www.xiami.com/profile', 'http://www.xiami.com/web')
 
-def days(headers_url,checkin_url):
+
+def days(headers_url, checkin_url):
     # 签到
     checkin_headers = {
-        'Referer': headers_url, 
+        'Referer': headers_url,
         'User-Agent': 'chrome/35.0'}
     checkin_request = urllib2.Request(checkin_url, None, checkin_headers)
     checkin_response = urllib2.urlopen(checkin_request).read()
@@ -45,6 +45,7 @@ def days(headers_url,checkin_url):
     except:
         return checkin_response
 
+
 def checkin(mid_response):
     # 获取签到连接
     url_pattern = re.compile(r'href="/web/checkin/id/(\d+)"')
@@ -52,11 +53,10 @@ def checkin(mid_response):
     return 'http://www.xiami.com/web/checkin/id/' + url_match
 
 
-
-query_days=login('your email','your password')
-if query_days>0:
-    print (u'已经签到%s天'%days('http://www.xiami.com/web',checkin(query_days)))
-elif query_days== 0:
+query_days = login('your email', 'your password')
+if query_days > 0:
+    print (u'已经签到%s天' % days('http://www.xiami.com/web', checkin(query_days)))
+elif query_days == 0:
     print(u'登录失败，请检查账号密码是否填写正确')
 else:
-    print (u'已经签到%s天'%abs(query_days))
+    print (u'已经签到%s天' % abs(query_days))
