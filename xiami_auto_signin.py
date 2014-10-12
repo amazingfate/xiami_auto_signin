24499925f1d776c4f70af1a69219c7386bc34c7c
jianguiqubaa <jianguiqubaa@gmail.com>
2014-10-12 17:03:16 +0800

    refactoring

--- xiami_auto_signin.py
+++ xiami_auto_signin.py
@@ -1,61 +1,69 @@
+#_*_conding:utf-8_*_
 # coding=utf-8
 import urllib2
 import urllib
 import re
 
-login_url = 'http://www.xiami.com/web/login'
 
-login_data = urllib.urlencode({
-    'email': 'your email',
-    'password': 'your password',
-    'LoginButton': '登录',
-})
-
-login_headers = {
+def login(email,pw):
+    login_data_ = {
+    'email': email,
+    'password': pw,
+    'LoginButton': '登录'}
+    login_url = 'http://www.xiami.com/web/login'
+    login_headers = {
     'Referer': 'http://www.xiami.com/web',
-    'User-Agent': 'Chrome/35.0',
-}
+    'User-Agent': 'Chrome/35.0'}
 
-opener = urllib2.build_opener(urllib2.HTTPCookieProcessor)
-urllib2.install_opener(opener)
+    login_data = urllib.urlencode(login_data_)
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor)
+    urllib2.install_opener(opener)
 
-# 登录
-login_request = urllib2.Request(login_url, login_data, login_headers)
-login_response = urllib2.urlopen(login_request).read()
-
-
-# 跳转到签到页
-mid_headers = {
-    'Referer': 'http:www.xiami.com/profile', 'User-Agent': 'chrome/35.0'}
-mid_request = urllib2.Request('http://www.xiami.com/web', None, mid_headers)
-mid_response = urllib2.urlopen(mid_request).read()
+    # 登录
+    login_request = urllib2.Request(login_url, login_data, login_headers)
+    login_response = urllib2.urlopen(login_request).read()
 
 
-# 检查签到情况
-days_pattern = re.compile(r'已连续签到(\d+)天')
-if days_pattern.findall(mid_response):
-    result_match = days_pattern.findall(mid_response)[0]
-    print(u'已经签到' + result_match + u'天')
-else:
+    if 'email或者密码错误' in login_response:
+        return 0
+    else:
+        return days('http:www.xiami.com/profile','http://www.xiami.com/web')
+
+def days(headers_url,checkin_url):
+    # 签到
+    checkin_headers = {
+        'Referer': headers_url, 
+        'User-Agent': 'chrome/35.0'}
+    checkin_request = urllib2.Request(checkin_url, None, checkin_headers)
+    checkin_response = urllib2.urlopen(checkin_request).read()
+    # try:
+    #     checkin_response = urllib2.urlopen(checkin_request).read().decode('utf-8').encode('gbk')
+    # except:
+    #     pass
+    # with open('xiami.html','w') as f:
+    #     f.write(checkin_response)
+    # f.close()
+
+    # 检查签到结果
+    days_pattern = re.compile(r'已连续签到(\d+)天')
+    try:
+        result_match = days_pattern.findall(checkin_response)[0]
+        return -int(result_match)
+    except:
+        return checkin_response
+
+def checkin(mid_response):
     # 获取签到连接
     url_pattern = re.compile(r'href="/web/checkin/id/(\d+)"')
-    try:
-        url_match = url_pattern.findall(mid_response)[0]
-    except StandardError, e:
-        print (u'账户名或者密码错误，请重新填写')
-    else:
-        checkin_url = 'http://www.xiami.com/web/checkin/id/' + url_match
+    url_match = url_pattern.findall(mid_response)[0]
+    return 'http://www.xiami.com/web/checkin/id/' + url_match
 
-        # 签到
-        checkin_headers = {
-            'Referer': 'http://www.xiami.com/web', 'User-Agent': 'chrome/35.0'}
-        checkin_request = urllib2.Request(checkin_url, None, checkin_headers)
-        checkin_response = urllib2.urlopen(checkin_request).read()
 
-        # 检查签到结果
-        days_pattern = re.compile(r'已连续签到(\d+)天')
-        if days_pattern.findall(checkin_response):
-            result_match = days_pattern.findall(checkin_response)[0]
-            print (u'签到成功！' + u'已经签到' + result_match + u'天')
-        else:
-            print('fail')
+
+query_days=login('your email','your password')
+if query_days>0:
+    print (u'已经签到%s天'%days('http://www.xiami.com/web',checkin(query_days)))
+elif query_days== 0:
+    print(u'登录失败，请检查账号密码是否填写正确')
+else:
+    print (u'已经签到%s天'%abs(query_days))
